8c5e20f7233601bc8772c68b1e6ef57c9cec7728
LumosDhia <mdhiaaouina+github@outlook.com>
2026-04-11 19:27:54 +0000

    Add missing separator import fix

--- src/main/java/tn/esprit/ticket/TicketAchievementsController.java
+++ src/main/java/tn/esprit/ticket/TicketAchievementsController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import tn.esprit.services.TicketService;
