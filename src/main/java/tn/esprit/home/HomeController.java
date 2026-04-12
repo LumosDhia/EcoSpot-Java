263d1f24a5086f52436b53aab08439681dc5b0c0
wiemjouini <Wiemjouini77@gmail.com>
2026-04-12 12:58:07 +0000

    Add home feature package

---
+++ src/main/java/tn/esprit/home/HomeController.java
@@ -0,0 +1,13 @@
+package tn.esprit.home;
+
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+    @FXML
+    public void initialize() {
+        System.out.println("Home Screen Initialized");
+    }
+
+    // Methods for navigation buttons can be added here
+}
