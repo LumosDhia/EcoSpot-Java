8b1edb5944bf2e2617338c4bb0aea82dd23c694c
AzBazou <mohamedaziz.gasmi@esprit.tn>
2026-04-13 21:59:43 +0000

    Add process kill to script

--- run_app.bat
+++ run_app.bat
@@ -1,5 +1,9 @@
 @echo off
+echo Terminating existing EcoSpot instances...
+taskkill /F /IM java.exe /T 2>nul
+
 set "MVN_PATH=C:\Program Files\JetBrains\IntelliJ IDEA 2026.1\plugins\maven\lib\maven3\bin\mvn.cmd"
+
 if exist "%MVN_PATH%" (
     echo Launching EcoSpot using IntelliJ Maven...
     "%MVN_PATH%" javafx:run
